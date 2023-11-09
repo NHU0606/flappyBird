901c78c6c67191b1930192869b5bf11383ffc8dd
Phan Thị Quỳnh Như <82049190+NHU0606@users.noreply.github.com>
2023-11-09 23:15:47 +0700

    compare user nickname

--- assets/scripts/Entry.ts
+++ assets/scripts/Entry.ts
@@ -1,36 +1,46 @@
 import { RequestController } from './RequestController';
-import { _decorator, Component, EditBox, Node } from 'cc';
+import { _decorator, Component, EditBox, Node, Label, director } from 'cc';
 const { ccclass, property } = _decorator;
 
 @ccclass('Entry')
 export class Entry extends Component {
     @property({ type: RequestController })
-    private RequestController: RequestController;
-
-    @property({ type: Node })
-    private mainEntry: Node;
-
-    @property({ type: Node })
-    private nickName: Node;
+    private requestController: RequestController;
 
     @property({ type: EditBox })
     private nickNameLabel: EditBox;
+    private authData;
+    public static currentUser;
 
-    start() {
-        this.mainEntry.active = false;
-        this.nickName.active = true;
+    protected onLoad(): void {
+        let res =  this.requestController.get('/check_user');
+        res.then(r => {
+           if (r['status'] == 200) {
+            director.loadScene('Choose');
+           } else {
+            console.log("auth")
+           }
+        })
     }
-
-    private onClickBtnOk(): void {
-        this.mainEntry.active = true;
-        this.nickName.active = false;
-    }
-
-    private onIputSubmitted(): void {
+    private async onInputSubmitted(): Promise<void> {
         const nickname = this.nickNameLabel.string;
+        let formData = new FormData();
+        formData.append("username", nickname);
 
-        console.log(nickname);
+        let res =  this.requestController.post(formData, '/auth');
+        res.then(r => {
+           if (r['status'] == 401) {
+            console.log("auth")
+           } else {
+            director.loadScene('Choose');
+            Entry.currentUser = r['id'];
+           }
+        })
         
     }
+
+    onClickPlayBtn() {
+        this.onInputSubmitted(); 
+    } 
 }
 
